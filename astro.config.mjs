009cb9ad6309d8409a32c80d5fa29cb740311a59
jacquelinefranz <jacquelinefranz1505@gmail.com>
2025-12-09 12:57:11 +0100

    feat(bundmigration): Astro Konfiguration für Bun aktualisiert
    Installiert  und konfiguriert  für SSR mit Bun.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import node from '@astrojs/node';
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+  integrations: [vue()],
 });
